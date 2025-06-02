30824e239469bfa2a815556d5b27fdd8c401363c
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-06-02 10:48:01 -0300

    feat: ensure appointments are listed correctly

--- src/app/(protected)/appointments/page.tsx
+++ src/app/(protected)/appointments/page.tsx
@@ -1,37 +1,69 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageActions,
   PageContainer,
+  PageContent,
   PageDescription,
   PageHeader,
   PageHeaderContent,
   PageTitle,
 } from "@/components/ui/page-container";
 import { db } from "@/db";
-import { doctorsTable, patientsTable } from "@/db/schema";
+import { appointmentsTable, doctorsTable, patientsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
 
-import { NewAppointmentDialog } from "./_components/new-appointment-dialog";
+import AddAppointmentButton from "./_components/add-appointment-button";
+import { appointmentsTableColumns } from "./_components/table-columns";
 
-export default async function AppointmentsPage() {
-  const allDoctors = await db.select().from(doctorsTable);
-  const allPatients = await db.select().from(patientsTable);
+const AppointmentsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+
+  const [patients, doctors, appointments] = await Promise.all([
+    db.query.patientsTable.findMany({
+      where: eq(patientsTable.clinicId, session.user.clinic.id),
+    }),
+    db.query.doctorsTable.findMany({
+      where: eq(doctorsTable.clinicId, session.user.clinic.id),
+    }),
+    db.query.appointmentsTable.findMany({
+      where: eq(appointmentsTable.clinicId, session.user.clinic.id),
+      with: {
+        patient: true,
+        doctor: true,
+      },
+    }),
+  ]);
 
   return (
     <PageContainer>
-      <div className="flex items-center justify-between">
-        <PageHeader>
-          <PageHeaderContent>
-            <PageTitle>Agendamentos</PageTitle>
-            <PageDescription>
-              Gerencie os agendamentos do sistema
-            </PageDescription>
-          </PageHeaderContent>
-          <PageActions>
-            <NewAppointmentDialog doctors={allDoctors} patients={allPatients} />
-          </PageActions>
-        </PageHeader>
-      </div>
-
-      {/* Appointment list will be added here later */}
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Agendamentos</PageTitle>
+          <PageDescription>
+            Gerencie os agendamentos da sua clínica
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <AddAppointmentButton patients={patients} doctors={doctors} />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <DataTable data={appointments} columns={appointmentsTableColumns} />
+      </PageContent>
     </PageContainer>
   );
-}
+};
+
+export default AppointmentsPage;
