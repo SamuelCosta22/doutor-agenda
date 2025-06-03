5747a5fca030f0ec081c8761f485c38c73bf8e09
SamuelCosta22 <samuelgcostainstitucional@gmail.com>
2025-06-03 11:12:29 -0300

    feat: add today appointments table

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,10 @@
 import dayjs from "dayjs";
+import { Calendar } from "lucide-react";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
 import {
   PageActions,
   PageContainer,
@@ -14,6 +17,7 @@ import {
 import { getDashboard } from "@/data/get-dashboard";
 import { auth } from "@/lib/auth";
 
+import { appointmentsTableColumns } from "../appointments/_components/table-columns";
 import AppointmentsChart from "./_components/appointments-chart";
 import { DatePicker } from "./_components/date-picker";
 import StatsCards from "./_components/stats-cards";
@@ -54,6 +58,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
     totalPatients,
     totalDoctors,
     topDoctors,
+    todayAppointments,
     topSpecialties,
     dailyAppointmentsData,
   } = await getDashboard({
@@ -85,18 +90,38 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
         </PageActions>
       </PageHeader>
       <PageContent>
-        <StatsCards
-          totalRevenue={totalRevenue.total ? Number(totalRevenue.total) : null}
-          totalAppointments={totalAppointments.total}
-          totalPatients={totalPatients.total}
-          totalDoctors={totalDoctors.total}
-        />
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-[2.25fr_1fr]">
-          <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
-          <TopDoctors doctors={topDoctors} />
-        </div>
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-[2.25fr_1fr]">
-          <TopSpecialties topSpecialties={topSpecialties} />
+        <div className="space-y-4">
+          <StatsCards
+            totalRevenue={
+              totalRevenue.total ? Number(totalRevenue.total) : null
+            }
+            totalAppointments={totalAppointments.total}
+            totalPatients={totalPatients.total}
+            totalDoctors={totalDoctors.total}
+          />
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-[2.25fr_1fr]">
+            <AppointmentsChart dailyAppointmentsData={dailyAppointmentsData} />
+            <TopDoctors doctors={topDoctors} />
+          </div>
+          <div className="grid grid-cols-1 gap-4 lg:grid-cols-[2.25fr_1fr]">
+            <Card>
+              <CardHeader>
+                <div className="flex items-center gap-3">
+                  <Calendar className="text-muted-foreground" />
+                  <CardTitle className="text-base">
+                    Agendamentos de hoje
+                  </CardTitle>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <DataTable
+                  columns={appointmentsTableColumns}
+                  data={todayAppointments}
+                />
+              </CardContent>
+            </Card>
+            <TopSpecialties topSpecialties={topSpecialties} />
+          </div>
         </div>
       </PageContent>
     </PageContainer>
